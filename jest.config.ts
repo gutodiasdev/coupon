847688a1bfd898963804814991c5a5b43e236da2
Augusto Dias <me@gutodias.dev>
2022-09-27 00:45:29 -0300

    chore: add jest

--- jest.config.ts
+++ jest.config.ts
@@ -4,26 +4,10 @@
  */
 
 export default {
-  // All imported modules in your tests should be mocked automatically
-  // automock: false,
-
-  // Stop running tests after `n` failures
-  // bail: 0,
-
-  // The directory where Jest should store its cached dependency information
-  // cacheDirectory: "C:\\Users\\gutor\\AppData\\Local\\Temp\\jest",
-
-  // Automatically clear mock calls, instances, contexts and results before every test
   clearMocks: true,
-
-  // Indicates whether the coverage information should be collected while executing the test
-  collectCoverage: true,
-
-  // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-
-  // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage",
+  // collectCoverage: true,
+  // collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  // coverageDirectory: "coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
