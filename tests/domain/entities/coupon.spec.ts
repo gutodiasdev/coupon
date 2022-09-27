b1d3c5c9a811e036c911ebdbf02a6663c4dcfaab
Augusto Dias <me@gutodias.dev>
2022-09-27 00:42:52 -0300

    test: add Coupon entity validation tests

---
+++ tests/domain/entities/coupon.spec.ts
@@ -0,0 +1,48 @@
+import { Coupon, InputCoupon } from '../../../src/domain/entities/coupon'
+
+type OutputMakeSUT = {
+  sut: Coupon
+}
+
+const makeSUT = (params: InputCoupon): OutputMakeSUT => {
+  const sut = new Coupon({ ...params })
+
+  return {
+    sut
+  }
+}
+
+describe('Coupon Entity Unit Tests', () => {
+  let params = {
+    name: 'any_name',
+    store: 'any_store',
+    image: 'any_image',
+    oldPrice: 'any_oldPrice',
+    price: 'any_price',
+    type: 'any_type'
+  }
+
+  it('should garantee Coupon entity recieves all required parameters', () => {
+    expect(() => {
+      const coupon = makeSUT({ ...params, name: '' })
+    }).toThrowError('Name is required')
+
+    expect(() => {
+      const coupon = makeSUT({ ...params, store: '' })
+    }).toThrowError('Store is required')
+
+    expect(() => {
+      const coupon = makeSUT({ ...params, image: '' })
+    }).toThrowError('Image is required')
+
+    expect(() => {
+      const coupon = makeSUT({ ...params, type: '' })
+    }).toThrowError('Type is required')
+  })
+
+  it('should garantee a offer type coupon recieves current price as a parameter', () => {
+    expect(() => {
+      const coupon = makeSUT({ ...params, type: 'offer', currentPrice: '' })
+    }).toThrowError('Price is required for a offer type')
+  })
+})
